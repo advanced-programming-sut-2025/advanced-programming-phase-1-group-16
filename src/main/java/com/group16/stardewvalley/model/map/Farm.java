1dcfcde1dd937eaca713ba3ec0b839b291a8369c
atena <atenaraeisiiiiii@gmail.com>
2025-05-03 15:25:46 +0330

    position and getter setter are added

--- src/main/java/com/group16/stardewvalley/model/map/Farm.java
+++ src/main/java/com/group16/stardewvalley/model/map/Farm.java
@@ -3,8 +3,25 @@ package com.group16.stardewvalley.model.map;
 
 public class Farm {
     private FarmType type;
+    private Pos startPosition;
 
     public Farm(FarmType type) {
         this.type = type;
+    }
+
+    public FarmType getType() {
+        return type;
+    }
+
+    public void setType(FarmType type) {
+        this.type = type;
+    }
+
+    public Pos getStartPosition() {
+        return startPosition;
+    }
+
+    public void setStartPosition(Pos startPosition) {
+        this.startPosition = startPosition;
     }
 }
