b5c5c066fdeac79463e19b2d916ed6a895cc61e0
atena <atenaraeisiiiiii@gmail.com>
2025-05-11 21:13:46 +0330

    add some tiletype

--- src/main/java/com/group16/stardewvalley/model/map/TileType.java
+++ src/main/java/com/group16/stardewvalley/model/map/TileType.java
@@ -8,7 +8,8 @@ public enum TileType {
     Lake('~', "\u001B[33m"),
     GreenHouse('G', "\u001B[35m"),
     Cottage('C', "\u001B[31m"),
-    Quarry('Q', "\u001B[30m");
+    Quarry('Q', "\u001B[30m"),
+    Plowed('o', "");
 
     private final char symbol;
     private final String colorCode;
