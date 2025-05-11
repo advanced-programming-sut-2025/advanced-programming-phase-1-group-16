c646a9d4a23e71f75418a31f3c4f521334ea5f4c
atena <atenaraeisiiiiii@gmail.com>
2025-05-11 21:31:11 +0330

    finish plant function

--- src/main/java/com/group16/stardewvalley/controller/agriculture/AgricultureController.java
+++ src/main/java/com/group16/stardewvalley/controller/agriculture/AgricultureController.java
@@ -1,8 +1,11 @@
 package com.group16.stardewvalley.controller.agriculture;
 
 import com.group16.stardewvalley.model.Result;
-import com.group16.stardewvalley.model.agriculture.CropType;
-import com.group16.stardewvalley.model.agriculture.SeedType;
+import com.group16.stardewvalley.model.agriculture.*;
+import com.group16.stardewvalley.model.app.App;
+import com.group16.stardewvalley.model.map.Pos;
+import com.group16.stardewvalley.model.map.Tile;
+import com.group16.stardewvalley.model.map.TileType;
 
 
 public class AgricultureController {
@@ -73,7 +76,54 @@ public class AgricultureController {
             default:
                 return new Result(false, "Invalid direction");
         }
+        Pos playerPos = App.getActiveGame().getCurrentPlayer().getPosition();
+        if (playerPos.getX() + dirX < 0 || playerPos.getY() + dirY < 0 || playerPos.getX() + dirX > App.getActiveGame().getMapWidth() || playerPos.getY() + dirY > App.getActiveGame().getMapHeight()) {
+            return new Result(false, "Invalid direction");
+        }
+        Tile targetTile = App.getActiveGame().getMap()[playerPos.getY() + dirY][playerPos.getX() + dirX];
+        if (!targetTile.getType().equals(TileType.Plowed)) {
+            return new Result(false, "Shokhm nazadi dadash!");
+        }
+        switch (seedType.getType()) {
+            case "TREE":
+                TreeType treeType = findTreeTypeBySeed(seedType);
+                if (treeType == null) {
+                    return new Result(false, "Tree type not found");
+                }
+                Tree tree = new Tree(treeType);
+                targetTile.setTree(tree);
+                break;
+            case "CROP":
+                CropType cropType = findCropTypeBySeed(seedType);
+                if (cropType == null) {
+                    return new Result(false, "Crop type not found");
+                }
+                Crop crop = new Crop(cropType);
+                targetTile.setCrop(crop);
+                break;
+            default:
+                return new Result(false, "Invalid seed");
+                break;
+        }
 
+    }
+
+    private CropType findCropTypeBySeed(SeedType seedType) {
+        for (CropType crop : CropType.values()) {
+            if (crop.getSource().equals(seedType)) {
+                return crop;
+            }
+        }
+        return null;
+    }
+
+    private TreeType findTreeTypeBySeed(SeedType seedType) {
+        for (TreeType tree : TreeType.values()) {
+            if (tree.getSeed().equals(seedType)) {
+                return tree;
+            }
+        }
+        return null;
     }
 
     private CropType findCropTypeByName(String name) {
