0f0d32f582f01424aad44d790b3fb6d320bfe59b
atena <atenaraeisiiiiii@gmail.com>
2025-05-11 21:12:13 +0330

    add some seeds and add type to all

--- src/main/java/com/group16/stardewvalley/model/agriculture/SeedType.java
+++ src/main/java/com/group16/stardewvalley/model/agriculture/SeedType.java
@@ -1,97 +1,124 @@
 package com.group16.stardewvalley.model.agriculture;
 
+import com.group16.stardewvalley.model.time.Season;
+
+import java.util.List;
+
 public enum SeedType {
     // Permanent Stock
-    JOJA_COLA("Joja Cola", 75, Integer.MAX_VALUE),
-    ANCIENT_SEED("Ancient Seed", 500, 1),
-    GRASS_STARTER("Grass Starter", 125, Integer.MAX_VALUE),
-    SUGAR("Sugar", 125, Integer.MAX_VALUE),
-    WHEAT_FLOUR("Wheat Flour", 125, Integer.MAX_VALUE),
-    //اسمشو به رایس شوت تغییر دادم
-    RICE_SHOOT("Rice", 250, Integer.MAX_VALUE),
+    JOJA_COLA("Joja Cola",
+            75,
+            Integer.MAX_VALUE,
+            List.of(Season.Spring, Season.Summer, Season.Fall, Season.Winter),
+            "CROP"),
+    ANCIENT_SEED("Ancient Seed",
+            500,
+            1,
+            List.of(Season.Spring, Season.Summer, Season.Fall, Season.Winter),
+            "CROP"),
+    GRASS_STARTER("Grass Starter",
+            125,
+            Integer.MAX_VALUE,
+            List.of(Season.Spring, Season.Summer, Season.Fall, Season.Winter),
+            "CROP"),
+    SUGAR("Sugar",
+            125,
+            Integer.MAX_VALUE,
+            List.of(Season.Spring, Season.Summer, Season.Fall, Season.Winter),
+            "CROP"),
+    WHEAT_FLOUR("Wheat Flour",
+            125,
+            Integer.MAX_VALUE,
+            List.of(Season.Spring, Season.Summer, Season.Fall, Season.Winter),
+            "CROP"),
+    RICE("Rice",
+            250,
+            Integer.MAX_VALUE,
+            List.of(Season.Spring, Season.Summer, Season.Fall, Season.Winter),
+            "CROP"),
 
     // Spring Stock
-    PARSNIP_SEEDS("Parsnip Seeds", 25, 5),
-    BEAN_STARTER("Bean Starter", 75, 5),
-    CAULIFLOWER_SEEDS("Cauliflower Seeds", 100, 5),
-    POTATO_SEEDS("Potato Seeds", 62, 5),
-    STRAWBERRY_SEEDS("Strawberry Seeds", 100, 5),
-    TULIP_BULB("Tulip Bulb", 25, 5),
-    KALE_SEEDS("Kale Seeds", 87, 5),
-    //چرا اسپرینگ داره؟
-    COFFEE_BEANS_SPRING("Coffee Beans", 200, 1),
-    CARROT_SEEDS("Carrot Seeds", 5, 10),
-    RHUBARB_SEEDS("Rhubarb Seeds", 100, 5),
-    JAZZ_SEEDS("Jazz Seeds", 37, 5),
+    PARSNIP_SEEDS("Parsnip Seeds", 25, 5, List.of(Season.Spring), "CROP"),
+    BEAN_STARTER("Bean Starter", 75, 5, List.of(Season.Spring), "CROP"),
+    CAULIFLOWER_SEEDS("Cauliflower Seeds", 100, 5, List.of(Season.Spring), "CROP"),
+    POTATO_SEEDS("Potato Seeds", 62, 5, List.of(Season.Spring), "CROP"),
+    STRAWBERRY_SEEDS("Strawberry Seeds", 100, 5, List.of(Season.Spring), "CROP"),
+    TULIP_BULB("Tulip Bulb", 25, 5, List.of(Season.Spring), "CROP"),
+    KALE_SEEDS("Kale Seeds", 87, 5, List.of(Season.Spring), "CROP"),
+    COFFEE_BEANS("Coffee Beans", 200, 1, List.of(Season.Spring, Season.Summer), "CROP"),
+    CARROT_SEEDS("Carrot Seeds", 5, 10, List.of(Season.Spring), "CROP"),
+    RHUBARB_SEEDS("Rhubarb Seeds", 100, 5, List.of(Season.Spring), "CROP"),
+    JAZZ_SEEDS("Jazz Seeds", 37, 5, List.of(Season.Spring), "CROP"),
 
     // Summer Stock
-    TOMATO_SEEDS("Tomato Seeds", 62, 5),
-    PEPPER_SEEDS("Pepper Seeds", 50, 5),
-    WHEAT_SEEDS_SUMMER("Wheat Seeds", 12, 10),
-    SUMMER_SQUASH_SEEDS("Summer Squash Seeds", 10, 10),
-    RADISH_SEEDS("Radish Seeds", 50, 5),
-    MELON_SEEDS("Melon Seeds", 100, 5),
-    HOPS_STARTER("Hops Starter", 75, 5),
-    POPPY_SEEDS("Poppy Seeds", 125, 5),
-    SPANGLE_SEEDS("Spangle Seeds", 62, 5),
-    STARFRUIT_SEEDS("Starfruit Seeds", 400, 5),
-    //این چرا summer داره
-    COFFEE_BEANS_SUMMER("Coffee Beans", 200, 1),
-    SUNFLOWER_SEEDS_SUMMER("Sunflower Seeds", 125, 5),
+    TOMATO_SEEDS("Tomato Seeds", 62, 5, List.of(Season.Summer), "CROP"),
+    PEPPER_SEEDS("Pepper Seeds", 50, 5, List.of(Season.Spring), "CROP"),
+    WHEAT_SEEDS("Wheat Seeds", 12, 10, List.of(Season.Spring, Season.Fall), "CROP"),
+    SUMMER_SQUASH_SEEDS("Summer Squash Seeds", 10, 10, List.of(Season.Summer), "CROP"),
+    RADISH_SEEDS("Radish Seeds", 50, 5, List.of(Season.Spring), "CROP"),
+    MELON_SEEDS("Melon Seeds", 100, 5, List.of(Season.Spring), "CROP"),
+    HOPS_STARTER("Hops Starter", 75, 5, List.of(Season.Spring), "CROP"),
+    POPPY_SEEDS("Poppy Seeds", 125, 5, List.of(Season.Spring), "CROP"),
+    SPANGLE_SEEDS("Spangle Seeds", 62, 5, List.of(Season.Spring), "CROP"),
+    STARFRUIT_SEEDS("Starfruit Seeds", 400, 5, List.of(Season.Spring), "CROP"),
+    SUNFLOWER_SEEDS("Sunflower Seeds", 125, 5, List.of(Season.Spring, Season.Fall), "CROP"),
 
     // Fall Stock
-    CORN_SEEDS("Corn Seeds", 187, 5),
-    EGGPLANT_SEEDS("Eggplant Seeds", 25, 5),
-    PUMPKIN_SEEDS("Pumpkin Seeds", 125, 5),
-    BROCCOLI_SEEDS("Broccoli Seeds", 15, 5),
-    AMARANTH_SEEDS("Amaranth Seeds", 87, 5),
-    GRAPE_STARTER("Grape Starter", 75, 5),
-    BEET_SEEDS("Beet Seeds", 20, 5),
-    YAM_SEEDS("Yam Seeds", 75, 5),
-    BOK_CHOY_SEEDS("Bok Choy Seeds", 62, 5),
-    CRANBERRY_SEEDS("Cranberry Seeds", 300, 5),
-    //چرا فال داره
-    SUNFLOWER_SEEDS_FALL("Sunflower Seeds", 125, 5),
-    FAIRY_SEEDS("Fairy Seeds", 250, 5),
-    RARE_SEED("Rare Seed", 1000, 1),
-    WHEAT_SEEDS_FALL("Wheat Seeds", 12, 5),
+    CORN_SEEDS("Corn Seeds", 187, 5, List.of(Season.Fall), "CROP"),
+    EGGPLANT_SEEDS("Eggplant Seeds", 25, 5, List.of(Season.Fall), "CROP"),
+    PUMPKIN_SEEDS("Pumpkin Seeds", 125, 5, List.of(Season.Fall), "CROP"),
+    BROCCOLI_SEEDS("Broccoli Seeds", 15, 5, List.of(Season.Fall), "CROP"),
+    AMARANTH_SEEDS("Amaranth Seeds", 87, 5, List.of(Season.Fall), "CROP"),
+    GRAPE_STARTER("Grape Starter", 75, 5, List.of(Season.Fall), "CROP"),
+    BEET_SEEDS("Beet Seeds", 20, 5, List.of(Season.Fall), "CROP"),
+    YAM_SEEDS("Yam Seeds", 75, 5, List.of(Season.Fall), "CROP"),
+    BOK_CHOY_SEEDS("Bok Choy Seeds", 62, 5, List.of(Season.Fall), "CROP"),
+    CRANBERRY_SEEDS("Cranberry Seeds", 300, 5, List.of(Season.Fall), "CROP"),
+    FAIRY_SEEDS("Fairy Seeds", 250, 5, List.of(Season.Fall), "CROP"),
+    RARE_SEED("Rare Seed", 1000, 1, List.of(Season.Fall), "CROP"),
 
     // Winter Stock
-    POWDERMELON_SEEDS("Powdermelon Seeds", 20, 10),
-
+    POWDERMELON_SEEDS("Powdermelon Seeds", 20, 10,List.of(Season.Winter), "CROP"),
 
     //atena add for crop:
-    GARLIC_SEEDS("Garlic Seeds", 50, 5),
-    BLUEBERRY_SEEDS("Blueberry Seeds", 100, 5),
-    PINEAPPLE_SEEDS("Pineapple Seeds", 50, 5),
-    RED_CABBAGE_SEEDS("red Cabbage Seeds", 100, 5),
-    WHEAT_SEEDS("wheat Seeds", 125, 5),
-    ARTICHOKE_SEEDS("Archook Seeds", 50, 5),
+    GARLIC_SEEDS("Garlic Seeds", 50, 5, List.of(Season.Spring), "CROP"),
+    BLUEBERRY_SEEDS("Blueberry Seeds", 100, 5, List.of(Season.Spring), "CROP"),
+    PINEAPPLE_SEEDS("Pineapple Seeds", 50, 5, List.of(Season.Spring), "CROP"),
+    RED_CABBAGE_SEEDS("red Cabbage Seeds", 100, 5, List.of(Season.Spring), "CROP"),
+    ARTICHOKE_SEEDS("Archook Seeds", 50, 5, List.of(Season.Spring), "CROP"),
     //tree
-    APRICOT_SAPLING("Apricot Sapling", 50, 5),
-    CHERRY_SAPLING("Cherry Sapling", 50, 5),
-    BANANA_SAPLING("Banana Sapling", 50, 5),
-    MANGO_SAPLING("Mango Sapling", 50, 5),
-    ORANGE_SAPLING("Orange Sapling", 50, 5),
-    PEACH_SAPLING("Peach Sapling", 50, 5),
-    APPLE_SAPLING("Apple Sapling", 50, 5),
-    POMEGRANATE_SAPLING("Pomegranate Sapling", 50, 5),
-    ACORNS("Acorns", 50, 5),
-    MAPLE_SEEDS("Maple Seeds", 50, 5),
-    PINE_CONES("Pine Cones", 50, 5),
-    MAHOGANY_SEEDS("Mahogany Seeds", 50, 5),
-    MUSHROOM_TREE_SEEDS("Mushroom Tree", 50, 5),
-    MYSTIC_TREE_SEEDS("Mystic Tree", 50, 5);
+    APRICOT_SAPLING("Apricot Sapling", 50, 5, List.of(Season.Spring), "TREE"),
+    CHERRY_SAPLING("Cherry Sapling", 50, 5, List.of(Season.Spring), "TREE"),
+    BANANA_SAPLING("Banana Sapling", 50, 5, List.of(Season.Summer), "TREE"),
+    MANGO_SAPLING("Mango Sapling", 50, 5, List.of(Season.Summer), "TREE"),
+    ORANGE_SAPLING("Orange Sapling", 50, 5, List.of(Season.Summer), "TREE"),
+    PEACH_SAPLING("Peach Sapling", 50, 5, List.of(Season.Spring), "TREE"),
+    APPLE_SAPLING("Apple Sapling", 50, 5, List.of(Season.Spring), "TREE"),
+    POMEGRANATE_SAPLING("Pomegranate Sapling", 50, 5, List.of(Season.Spring), "TREE"),
+    ACORNS("Acorns", 50, 5, List.of(Season.Spring), "TREE"),
+    MAPLE_SEEDS("Maple Seeds", 50, 5, List.of(Season.Spring), "TREE"),
+    PINE_CONES("Pine Cones", 50, 5, List.of(Season.Spring), "TREE"),
+    MAHOGANY_SEEDS("Mahogany Seeds", 50, 5, List.of(Season.Spring), "TREE"),
+    MUSHROOM_TREE_SEEDS("Mushroom Tree", 50, 5, List.of(Season.Spring), "TREE"),
+    MYSTIC_TREE_SEEDS("Mystic Tree", 50, 5, List.of(Season.Spring), "TREE");
 
 
     private final String name;
     private final int price;
     private final int dailyLimit;
+    private final List<Season> availableSeasons;
+    private final String type;
 
-    SeedType(String name, int price, int dailyLimit) {
+    SeedType(String name, int price, int dailyLimit, List<Season> availableSeasons, String type) {
         this.name = name;
         this.price = price;
         this.dailyLimit = dailyLimit;
+        this.availableSeasons = availableSeasons;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
     }
 
     public String getName() {
@@ -104,6 +131,10 @@ public enum SeedType {
 
     public int getDailyLimit() {
         return dailyLimit;
+    }
+
+    public List<Season> getAvailableSeasons() {
+        return availableSeasons;
     }
 
 }
