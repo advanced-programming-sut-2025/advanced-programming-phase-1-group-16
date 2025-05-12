81976c0e3ab4cd4ac2ef285ac7f09d499f34491e
fatemeh-nilforoushan <fnilforoushan06@gmail.com>
2025-05-12 07:27:40 +0330

    add items have been changed

--- src/main/java/com/group16/stardewvalley/model/Inventory.java
+++ src/main/java/com/group16/stardewvalley/model/Inventory.java
@@ -1,8 +1,10 @@
 package com.group16.stardewvalley.model;
 
 
+import com.group16.stardewvalley.model.Things.Item;
 import com.group16.stardewvalley.model.Tools.Gadget;
 import com.group16.stardewvalley.model.crafting.CraftingRecipes;
+import com.group16.stardewvalley.model.user.BackPackType;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,26 +12,34 @@ import java.util.Map;
 
 public class Inventory {
     private Map<Gadget, Integer> tools;
+    private Map<Item, Integer> items;
     private int capacity;
     private ArrayList<CraftingRecipes> craftingRecipes;
+    private BackPackType backPackType;
 
     public Inventory() {
         this.tools = new HashMap<>();
+        this.items = new HashMap<>();
+        this.backPackType = BackPackType.Base_Pack;
+
     }
 
-    public void addTool(Gadget gadget, int count) {
+    public Result addTool(Gadget gadget, int count) {
+        if (isFull()) {
+            return new Result(false, "Oops! Your backpack is completely full ");
+        }
         String name = gadget.getName();
         tools.put(gadget, tools.getOrDefault(gadget, 0) + count);
+        return new Result(true, gadget.getName() + " added to inventory successfully");
     }
 
-    public Gadget findToolByName(String name) {
-        for (Map.Entry<Gadget, Integer> entry : tools.entrySet()) {
-            Gadget gadget = entry.getKey();
-            if (gadget.getName().equalsIgnoreCase(name)) {
-                return gadget;
-            }
+    public Result addItem(Item item, int count) {
+        if (isFull()) {
+            return new Result(false, "Oops! Your backpack is completely full ");
         }
-        return null;
+        String name = item.getName();
+        items.put(item, items.getOrDefault(item, 0) + count);
+        return new Result(true, item.getName() + " added to inventory successfully");
     }
 
     public ArrayList<CraftingRecipes> getCraftingRecipes() {
@@ -44,7 +54,35 @@ public class Inventory {
         this.craftingRecipes.add(craftingRecipes);
     }
 
+    public Map<Item, Integer> getItems() {
+        return items;
+    }
+
+    public BackPackType getBackPackType() {
+        return backPackType;
+    }
+
+    public Gadget findToolByName(String name) {
+        for (Map.Entry<Gadget, Integer> entry : tools.entrySet()) {
+            Gadget gadget = entry.getKey();
+            if (gadget.getName().equalsIgnoreCase(name)) {
+                return gadget;
+            }
+        }
+        return null;
+    }
+
     public Map<Gadget, Integer> getTools() {
         return tools;
+    }
+
+    public boolean isFull() {
+        return getTotalItemsCount() >= backPackType.getCapacity();
+    }
+
+    private int getTotalItemsCount() {
+        int toolCount = tools.values().stream().mapToInt(Integer::intValue).sum();
+        int itemCount = items.values().stream().mapToInt(Integer::intValue).sum();
+        return toolCount+ itemCount;
     }
 }
