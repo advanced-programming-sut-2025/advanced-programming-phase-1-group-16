254190c469f74547a26e871e192f89dede68568d
fatemeh-nilforoushan <fnilforoushan06@gmail.com>
2025-05-17 18:56:24 +0330

    last ones

--- src/main/java/com/group16/stardewvalley/model/crafting/artisan/ArtisanGood.java
+++ src/main/java/com/group16/stardewvalley/model/crafting/artisan/ArtisanGood.java
@@ -4,13 +4,23 @@ import com.group16.stardewvalley.model.items.Item;
 
 public class ArtisanGood extends Item {
     ArtisanGoodType type;
+    int startHour;
 
-    public ArtisanGood(String name, ArtisanGoodType type) {
+    public ArtisanGood(String name, ArtisanGoodType type, int startHour) {
         super(name);
         this.type = type;
+        this.startHour = startHour;
     }
 
     public ArtisanGoodType getType() {
         return type;
+    }
+
+    public int getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(int startHour) {
+        this.startHour = startHour;
     }
 }
